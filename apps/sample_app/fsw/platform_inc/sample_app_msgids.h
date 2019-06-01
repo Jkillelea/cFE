98eb22fbc28990e602960fb85a6cf6d197d83758
Jacob Killelea <Jkillelea@protonmail.ch>
2019-05-31 18:33:43 -0700

    misc formatting

--- apps/sample_app/fsw/platform_inc/sample_app_msgids.h
+++ apps/sample_app/fsw/platform_inc/sample_app_msgids.h
@@ -14,7 +14,7 @@
 
 #define SAMPLE_APP_CMD_MID            	0x1882
 #define SAMPLE_APP_SEND_HK_MID        	0x1883
-#define SAMPLE_APP_HK_TLM_MID		0x0883
+#define SAMPLE_APP_HK_TLM_MID		    0x0883
 
 #endif /* _sample_app_msgids_h_ */
 
